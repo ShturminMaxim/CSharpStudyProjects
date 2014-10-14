45d813fccbf85518d4cde7881caccddb3cc22abc
Bohuslavskyi Olexandr <kenobit.alex@gmail.com>
2014-10-14 20:37:23 +0300

    Add cubes Game

---
+++ BoxGame/BoxGame/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("BoxGame")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BoxGame")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("00e96a8c-52e1-44bd-b7f1-a76d67e0ec95")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Collections/Collections/Program.cs
+++ Collections/Collections/Program.cs
@@ -8,8 +8,47 @@ namespace Collections
 {
     class Program
     {
+        //static int F(int a, int b) 
+        //{
+        //    return a + b;
+        //}
+        //static float F(float a, float b)
+        //{
+        //    return 1;
+        //}
+        //static int F(int[] arr) {
+        //    int sum = 0;
+        //    for (int i = 0; i < arr.length; i++)
+        //    {
+        //        sum += arr[i];
+        //    }
+        //    return summ;
+        //}
+        //static void Myfunc(ref int a, ref int b) {
+
+        //    int t = a;
+        //    b = a;
+        //    a = t;
+
+        //}
+        static void Myfunc2(int[] arr) {
+            arr[1] = 2;
+
+        }
         static void Main(string[] args)
         {
+            //int[] a = { 1, 2, 3, 4, 5, 6 };
+            //Console.WriteLine(F(a));
+
+            int[] a = { 0, 0, 0, };
+            
+            Myfunc2(a);
+            
+            Console.WriteLine(a[1]);
+            
+            Console.ReadLine();
+
+
         }
     }
 }
