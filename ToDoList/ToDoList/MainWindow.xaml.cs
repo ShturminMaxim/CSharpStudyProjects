2c7c43de92e07e65f15a875edc9c038adfe7dfb3
Shturmin Maxim <koyotka@yahoo.com>
2015-01-29 20:19:35 +0200

    work with wpf

--- ToDoList/ToDoList/MainWindow.xaml.cs
+++ ToDoList/ToDoList/MainWindow.xaml.cs
@@ -23,26 +23,31 @@ namespace ToDoList
     public partial class MainWindow : Window
     {
         List<TodoItem> todoItems = new List<TodoItem>();
+        DateTime chosenFilterDate = DateTime.Today; 
 
         public MainWindow()
         {
             InitializeComponent();
             downloadData();
+            showCurrentDate();
         }
-
+        private void showCurrentDate() {
+            string todayDate = DateTime.Today.ToString("d");
+            CurrDate.Text = todayDate;
+            updateDates(todayDate);
+        }
         private void downloadData() {
+            FileStream fs = new FileStream("todos.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
             try
             {
-                FileStream fs = new FileStream("todos.dat", FileMode.OpenOrCreate, FileAccess.ReadWrite);
                 BinaryFormatter bf = new BinaryFormatter();
                 todoItems = (List<TodoItem>)bf.Deserialize(fs);
                 fs.Close();
-
                 updateList();
-
             }
             catch (Exception ex)
             {
+                fs.Close();
                 //MessageBox.Show(ex.Message);
             }
         }
@@ -64,7 +69,11 @@ namespace ToDoList
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            addElem();
+            if (AddItemPopUp.IsVisible)
+                AddItemPopUp.Visibility = System.Windows.Visibility.Hidden;
+            else
+                AddItemPopUp.Visibility = System.Windows.Visibility.Visible;
+            //addElem();
         }
         private void NewTodoText_KeyUp(object sender, KeyEventArgs e)
         {       
@@ -79,11 +88,12 @@ namespace ToDoList
             if (newTodoText.Length > 0)
             {
                 int id = todoItems.Count();
-                TodoItem newTodo = new TodoItem(newTodoText, id);
+                TodoItem newTodo = new TodoItem(newTodoText, id, chosenFilterDate);
                 todoItems.Add(newTodo);
                 NewTodoText.Text = "";
             }
             updateList();
+            AddItemPopUp.Visibility = System.Windows.Visibility.Hidden;
         }
 
         private void updateList() {
@@ -91,28 +101,32 @@ namespace ToDoList
             int position = 0;
             foreach (var item in todoItems) 
             {
-                TextBlock textBlock = new TextBlock();
-                Button delBtn = new Button();
+                bool isChoseNdate = item.CreationDate.Equals(chosenFilterDate);
+                if (isChoseNdate == true) {
+                    TextBlock textBlock = new TextBlock();
+                    Button delBtn = new Button();
 
-                textBlock.Text = item.TodoName;
-                textBlock.Margin = new Thickness(0, position, 0, 0);
-                
+                    textBlock.Text = item.TodoName;
+                    textBlock.Margin = new Thickness(0, position, 0, 0);
 
-                delBtn.Tag = item.Id;
-                delBtn.Height = 22;
-                delBtn.Width = 80;
-                delBtn.Content = "del";
-                delBtn.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
-                delBtn.VerticalAlignment = System.Windows.VerticalAlignment.Top;
-                delBtn.Margin = new Thickness(0, position, 0, 0);
-                delBtn.Click += delElem;
 
-                ToDoList.Children.Add(textBlock);
-                ToDoList.Children.Add(delBtn);
+                    delBtn.Tag = item.Id;
+                    delBtn.Height = 22;
+                    delBtn.Width = 80;
+                    delBtn.Content = "del";
+                    delBtn.HorizontalAlignment = System.Windows.HorizontalAlignment.Right;
+                    delBtn.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                    delBtn.Margin = new Thickness(0, position, 0, 0);
+                    delBtn.Click += delElem;
 
-                position += 24;
+                    ToDoList.Children.Add(textBlock);
+                    ToDoList.Children.Add(delBtn);
+
+                    position += 24;
+                }
+
             }
-            saveData();
+            //saveData();
         }
 
         private void delElem(object sender, RoutedEventArgs e)
@@ -135,6 +149,54 @@ namespace ToDoList
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void CalendarDate_DataContextChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+          //  MessageBox.Show("tete");
+        }
+
+        private void CalendarDate_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DateTime dt = DateTime.Today;
+
+            bool isI = dt.Date.Equals(CalendarDate.SelectedDate);
+            //MessageBox.Show(isI.ToString());
+            //MessageBox.Show(DateTime.Today.ToString("d"));
+            DateTime selectedDate = CalendarDate.SelectedDate.Value;
+            
+            chosenFilterDate = selectedDate;
+            updateDates(selectedDate.ToString("d"));
+            CalendarDate.Visibility = System.Windows.Visibility.Hidden;
+            updateList();
+            //MessageBox.Show(selectedDate.Value.Date.ToShortDateString());
+        }
+        private void updateDates(string date) {
+            DateForNewTask.Text = date;
+            FilterDate.Text = date; 
+        }
+        private void FilterByDate_Click(object sender, RoutedEventArgs e)
+        {
+            if(CalendarDate.IsVisible)
+                CalendarDate.Visibility = System.Windows.Visibility.Hidden;
+            else
+                CalendarDate.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            saveData();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            CalendarDate.Visibility = System.Windows.Visibility.Hidden;
+            AddItemPopUp.Visibility = System.Windows.Visibility.Hidden;
+        }
+
+        private void AddElemBtn_Click(object sender, RoutedEventArgs e)
+        {
+            addElem();
         }
     }
 }
--- ToDoList/ToDoList/TodoItem.cs
+++ ToDoList/ToDoList/TodoItem.cs
@@ -12,11 +12,13 @@ namespace ToDoList
         public string TodoName {set;get;}
         public bool IsDone {set;get;}
         public int Id { set; get; }
+        public DateTime CreationDate { set; get; }
 
-        public TodoItem(string text, int id) {
+        public TodoItem(string text, int id, DateTime date) {
             this.TodoName = text;
             this.IsDone = false;
             this.Id = id;
+            this.CreationDate = date;
         }
     }
 }
