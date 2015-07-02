fb6eb2bab325cde3d32647adfc860bfe06ad63c5
Shturmin Maxim <kenobit.alex@gmail.com>
2015-07-02 20:45:11 +0300

    asp work

--- ASP_Library/ASP_Library/Controllers/BooksController.cs
+++ ASP_Library/ASP_Library/Controllers/BooksController.cs
@@ -1,5 +1,6 @@
 ﻿using ASP_Library.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,7 +23,115 @@ namespace ASP_Library.Controllers
             return View();
         }
 
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            var textSearch = form["textSearch"];
+            //var type = form["searchType"];
+            //var listBooks = db.Books.Where(b => b.Quantity > 0).Distinct();
+            var listBooks = db.Books.Where(b => b.Quantity > 0).Distinct();
+            var checkboxes = form["searchType"].Split(',');
+            var changed = false;
+            //ViewBag.Checkboxes = checkboxes;
 
+            //listBooks.ToList().Concat(db.Books.Where(b => b.Authors.FirstName.Contains(textSearch)).ToList());
+
+            for (int i = 0; i < checkboxes.Count(); i++)
+            {
+                var item = checkboxes[i];
+                switch (item)
+                {
+                    case "author":
+                        if (changed == false) {
+                            listBooks = db.Books.Where(b => b.Authors.FirstName.Contains(textSearch));
+                        } else {
+                            changed = true;
+                            listBooks.Union(db.Books.Where(b => b.Authors.FirstName.Contains(textSearch)));
+                        }
+                        break;
+                    case "category":
+                        if (changed == false)
+                        {
+                            listBooks = db.Books.Where(b => b.Authors.FirstName.Contains(textSearch));
+                        }
+                        else
+                        {
+                            changed = true;
+                            listBooks.Union(db.Books.Where(b => b.Categories.Name.Contains(textSearch)));
+                        }
+                        
+                        break;
+                    case "theme":
+                        if (changed == false)
+                        {
+                            listBooks = db.Books.Where(b => b.Authors.FirstName.Contains(textSearch));
+                        }
+                        else
+                        {
+                            changed = true;
+                            listBooks.Union(db.Books.Where(b => b.Themes.Name.Contains(textSearch)));
+                        }
+                       
+                        break;
+                    case "press":
+                        if (changed == false)
+                        {
+                            listBooks = db.Books.Where(b => b.Authors.FirstName.Contains(textSearch));
+                        }
+                        else
+                        {
+                            changed = true;
+                            listBooks.Union(db.Books.Where(b => b.Press.Name.Contains(textSearch)));
+                        }
+                        
+                        break;
+                    case "name":
+                        if (changed == false)
+                        {
+                            listBooks = db.Books.Where(b => b.Authors.FirstName.Contains(textSearch));
+                        }
+                        else
+                        {
+                            changed = true;
+                            listBooks.Union(db.Books.Where(b => b.Name.Contains(textSearch)));
+                        }
+                        
+                        break;
+                }
+            }
+            
+            //switch (type)
+            //{
+            //    case "author":
+            //        listBooks = db.Books.Where(b => b.Authors.FirstName.Contains(textSearch));
+            //        break;
+            //    case "category":
+            //        listBooks = db.Books.Where(b => b.Categories.Name.Contains(textSearch));
+            //        break;
+            //    case "theme":
+            //        listBooks = db.Books.Where(b => b.Themes.Name.Contains(textSearch));
+            //        break;
+            //    case "press":
+            //        listBooks = db.Books.Where(b => b.Press.Name.Contains(textSearch));
+            //        break;
+            //    case "name":
+            //        listBooks = db.Books.Where(b => b.Name.Contains(textSearch));
+            //        break;
+            //}
+   
+            ViewBag.ListBooks = listBooks.ToList(); 
+
+            return View();
+        }
+
+        //[HttpGet]
+        //public ActionResult Index(string bookName)
+        //{
+        //    var listBooks = db.Books.Where(b => b.Name.Contains(bookName));
+        //    ViewBag.ListBooks = listBooks.ToList();
+
+        //    return View();
+        //}
 
         //
         // GET: /Books/Details/5
