bb7a5c49cda1f51e76cabc6ebb634f7266dfbf2d
Shturmin Maxim <kenobit.alex@gmail.com>
2015-07-09 20:38:30 +0300

    wip ASP

---
+++ ASP_Library/ASP_Library/Models/userData.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASP_Library.Models
+{
+    public class userData
+    {
+        public DateTime? DateIn { get; set; }
+        public DateTime DateOut { get; set; }
+        public Books Books;
+        public string LastName { get; set; }
+    }
+}
